dfa11c2e82c9950cd87ee46955d9a3dc5716c8ce
Guilherme <guizin.coelho@gmail.com>
2018-08-10 17:15:11 -0300

    Create App Porject

---
+++ TWKamu/TWKamu/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TWKamu
+//
+//  Created by Guilherme Coelho on 10/08/18.
+//  Copyright © 2018 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+  
+}
+
---
+++ TWKamu/TWKamu/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TWKamu
+//
+//  Created by Guilherme Coelho on 10/08/18.
+//  Copyright © 2018 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
